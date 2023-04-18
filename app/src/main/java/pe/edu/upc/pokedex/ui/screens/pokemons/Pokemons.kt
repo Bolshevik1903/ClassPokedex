07c1fd52616c89d99cb2e9782d3eee7db5d30a77
Usuario de proyectos <proyecto@h51-02.upc.edu.pe>
2023-04-18 14:19:14 -0500

    Added changes sthetic for principal screen

--- app/src/main/java/pe/edu/upc/pokedex/ui/screens/pokemons/Pokemons.kt
+++ app/src/main/java/pe/edu/upc/pokedex/ui/screens/pokemons/Pokemons.kt
@@ -6,7 +6,11 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Card
 import androidx.compose.material.ExperimentalMaterialApi
@@ -16,6 +20,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
@@ -25,18 +30,21 @@ import pe.edu.upc.pokedex.ui.theme.PokedexTheme
 
 @Composable
 fun Pokemon(pokemons: List<Pokemon>, selectPokemon: (String) -> Unit) {
-    LazyColumn(){
+    LazyVerticalGrid(columns = GridCells.Fixed(2)) {
 
-        items(pokemons){
-            PokemonItem(it, selectPokemon)
+        itemsIndexed(pokemons){index, item ->
+            PokemonItem(item, index){
+                selectPokemon("${index + 1}")
+            }
         }
     }
 }
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
-fun PokemonItem(pokemon: Pokemon, selectPokemon: (String) -> Unit) {
+fun PokemonItem(pokemon: Pokemon, index: Int, selectPokemon: (String) -> Unit) {
     Card(
+        elevation = 2.dp,
         onClick = {
             selectPokemon(getId(pokemon.url))
         }) {
@@ -45,9 +53,9 @@ fun PokemonItem(pokemon: Pokemon, selectPokemon: (String) -> Unit) {
             verticalAlignment = Alignment.CenterVertically
         ) {
             AsyncImage(
-                model = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/373.png",
-                contentDescription = "Pokemon's sprite",
-                modifier = Modifier.clip(CircleShape).background(Color.Green)
+                model = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index + 1}.png",
+                contentDescription = null,
+                contentScale = ContentScale.FillBounds
             )
             Text(text = pokemon.name)
         }

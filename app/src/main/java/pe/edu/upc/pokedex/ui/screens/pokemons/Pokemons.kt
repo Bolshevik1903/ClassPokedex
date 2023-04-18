bcf0ef088128e5e10e4b9cb1abe6051d12828c71
Usuario de proyectos <proyecto@h51-02.upc.edu.pe>
2023-04-18 14:03:44 -0500

    Added image on principal screen

--- app/src/main/java/pe/edu/upc/pokedex/ui/screens/pokemons/Pokemons.kt
+++ app/src/main/java/pe/edu/upc/pokedex/ui/screens/pokemons/Pokemons.kt
@@ -1,16 +1,25 @@
 package pe.edu.upc.pokedex.ui.screens.pokemons
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Card
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.compose.rememberImagePainter
 import pe.edu.upc.pokedex.data.remote.Pokemon
 import pe.edu.upc.pokedex.ui.theme.PokedexTheme
 
@@ -35,6 +44,11 @@ fun PokemonItem(pokemon: Pokemon, selectPokemon: (String) -> Unit) {
             modifier = Modifier.fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically
         ) {
+            AsyncImage(
+                model = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/373.png",
+                contentDescription = "Pokemon's sprite",
+                modifier = Modifier.clip(CircleShape).background(Color.Green)
+            )
             Text(text = pokemon.name)
         }
     }
